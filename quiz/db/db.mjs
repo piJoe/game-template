0bda85d2445b291d7a80c9a4ad661d7c6bef2e49
piJoe <raoul@dagalaxy.net>
2023-01-21 13:47:33 +0100

    make local copy of sqlite on start, improves performance

--- quiz/db/db.mjs
+++ quiz/db/db.mjs
@@ -1,7 +1,9 @@
 import sqlite3 from "sqlite3";
 import flat from "flat";
+import fs from "fs";
 
-const db = new sqlite3.Database("/animedb.sqlite");
+fs.copyFileSync("/animedb.sqlite", "/animedb_copy.sqlite");
+const db = new sqlite3.Database("/animedb_copy.sqlite", sqlite3.OPEN_READONLY);
 
 export function queryFirst(query, ...params) {
   return new Promise((res, rej) => {
@@ -16,8 +18,10 @@ export function queryFirst(query, ...params) {
 }
 
 export function queryAll(query, ...params) {
+  console.time("query");
   return new Promise((res, rej) => {
     db.all(query, params, (err, rows) => {
+      console.timeEnd("query");
       if (err) {
         return rej(err);
       }
